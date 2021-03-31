c0e7c84170a877d9ed18d11bfa793715ee1ff97a
gpzlx1 <gpzlx1@mail.ustc.edu.cn>
2021-03-31 16:26:40 +0800

    finish reorder

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import math
 import random
 import numpy as np
 from tqdm import tqdm
+import queue
 
 def random_gen(num_node, num_edge):
     num_node = int(num_node)
     num_edge = int(num_edge)
-    src = []
-    dst = []
     edges = set()
     for _ in tqdm(range(num_edge)):
         x = random.randint(0, num_node)
@@ -15,9 +14,7 @@ def random_gen(num_node, num_edge):
         if (x, y) in edges:
             continue
         edges.add((x,y))
-        src.append(x)
-        dst.append(y)
-    return src, dst
+    return edges
 
 #Kronecker Prob kernel KPK = []
 #num_node must be 2^m
@@ -33,8 +30,6 @@ def kronecker_gen(num_node, num_edge, prob_kernel = [0.99, 0.54, 0.49, 0.13]):
     num_node = 2 ** math.ceil(math.log2(num_node))
     M = int(math.log2(num_node))
 
-    src = []
-    dst = []
     edges = set()
 
     for _ in tqdm(range(num_edge)):
@@ -59,6 +54,46 @@ def kronecker_gen(num_node, num_edge, prob_kernel = [0.99, 0.54, 0.49, 0.13]):
             #print(x,y)
             continue
         edges.add((x,y))
-        src.append(x)
-        dst.append(y)
-    return src, dst
+    return edges
+
+def Gorder(edges, order = 'out'):      
+    graph_dict = {}
+    for src, dst in edges:
+
+        if order == 'in':
+            src, dst = dst, src
+
+        if src in graph_dict:
+            graph_dict[src].append(dst)
+        else:
+            graph_dict[src] = [dst]
+
+    unvisited = [ i for i in graph_dict.keys() ]
+    unvisited.sort(key=lambda x:len(graph_dict[x]), reverse=True)
+    q = queue.Queue()
+    visited = []
+
+    for src in unvisited:
+        if src in visited:
+            continue
+        q.put(src)
+        while not q.empty():
+            src = q.get()
+            if src in visited:
+                continue
+            visited.append(src)
+            dsts = graph_dict.get(src, [])
+            for dst in dsts:
+                q.put(dst)
+
+    map_dict = {}
+    for index, src in enumerate(visited):
+        map_dict[src] = index
+
+    order_edges = set()
+    for src, dst in edges:
+        order_edges.add((map_dict.get(src, src), map_dict.get(dst, dst)))
+    return order_edges
+    
+
+
